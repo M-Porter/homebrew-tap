015c5ffcf1a480ef9101d56603b11520b8ae9611
Matt Porter <mtp5129@gmail.com>
2021-11-14 22:33:14 -0700

    remove log

--- jumparound.rb
+++ jumparound.rb
@@ -3,16 +3,16 @@ class Jumparound < Formula
 
   desc "Quickly jump around between your projects."
   homepage "https://github.com/M-Porter/jumparound"
-  url "https://github.com/M-Porter/jumparound/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "a30418d892b71f2ca2a399bfed0088b22bb9ca9b977dfa2db9dab79d6cb9c30f"
+  url "https://github.com/M-Porter/jumparound/archive/refs/tags/0.2.0.tar.gz"
+  sha256 "4eac6124961c177cf0b0fd9f9abb229bdf84f18e707a46920268d9ac2cf824e7"
   license "MIT"
   head "https://github.com/M-Porter/jumparound.git", branch: "main"
 
   depends_on "python@3.9"
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/21/83/308a74ca1104fe1e3197d31693a7a2db67c2d4e668f20f43a2fca491f9f7/click-8.0.1.tar.gz"
-    sha256 "8c04c11192119b1ef78ea049e0a6f0463e4c48ef00a30160c704337586f3ad7a"
+    url "https://files.pythonhosted.org/packages/f4/09/ad003f1e3428017d1c3da4ccc9547591703ffea548626f47ec74509c5824/click-8.0.3.tar.gz"
+    sha256 "410e932b050f5eed773c4cda94de75971c89cdb3155a72a0831139a79e5ecb5b"
   end
 
   resource "colorama" do
